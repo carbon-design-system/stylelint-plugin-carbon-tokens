5b59ed29766a4039b5a487a7cff16b42dc1a2ae9
Lee Chase <lee.chase@uk.ibm.com>
2020-04-17 16:31:02 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,93 @@
+# stylelint-carbon-use
+
+## Installation
+
+```
+npm install stylelint-carbon-use
+```
+
+## Usage
+
+Add it to your stylelint config `plugins` array, then add `"carbon-use"` to your rules,
+specifying the property for which you want to check the usage of variable.
+
+Like so:
+
+```js
+// .stylelintrc
+{
+  "plugins": [
+    "stylelint-carbon-use"
+  ],
+  "rules": {
+    // ...
+    "plugin/carbon-use": "color",
+    // ...
+  }
+}
+```
+
+#### Multiple properties
+
+Multiple properties can be watched by passing them inside array. Regex can also be used inside arrays.
+
+```js
+// .stylelintrc
+"rules": {
+  // ...
+  "plugin/carbon-use": [["/color/", "background-color", "box-shadow", "border"]],
+  // ...
+}
+```
+
+#### Regex support
+
+Passing a regex will watch the variable usage for all matching properties. This rule will match all CSS properties while ignoring Sass and Less variables.
+
+```js
+// .stylelintrc
+"rules": {
+  // ...
+  "plugin/carbon-use": "/color/",
+  // ...
+}
+```
+
+#### Options
+
+Passing `ignoreValues` option, you can accpet values which are exact same string or matched by Regex
+
+```js
+// .stylelintrc
+"rules": {
+  // ...
+  "plugin/carbon-use": [["/color/", "font-size", { ignoreValues: ["transparent", "inherit", "initial", "/[^(]+/"] }]],
+  // ...
+}
+```
+
+## Details
+
+Carbon components has tokens for use with color, theming, timing, spacing, typography etc. Use this linter in order to encourage / verify thier use.
+
+```scss
+$ui-01: #f4f4f4;
+
+.foo {
+  color: #f4f4f4; // Ooh, that's not a carbon token 👋
+}
+```
+
+### Supported scss variables
+
+Scss variables using '\$' notation and perhaps carbon functions
+
+```scss
+// Simple variables
+$ui-01: #f4f4f4;
+
+color: $some-carbon-token;
+
+// Using carbon functions
+background-color: get-light-value($ui-01);
+```
---
+++ index.js
@@ -0,0 +1,197 @@
+const stylelint = require("stylelint");
+
+//
+// load carbon theme tokens
+//
+const carbonThemes = require("@carbon/themes");
+// console.dir(carbonThemes.tokens.colors);
+const themesTokens = carbonThemes.tokens.colors.map((token) =>
+  carbonThemes.formatTokenName(token)
+);
+const themeFunctions = ["get-light-value"];
+
+const ruleName = "plugin/carbon-use";
+
+const messages = stylelint.utils.ruleMessages(ruleName, {
+  expected: function expected(property) {
+    return 'Expected carbon token for "' + property + '".';
+  },
+});
+
+// Store the variables in object
+const variables = {};
+
+/**
+ * Returns boolean wether a string should be parsed as regex or not
+ *
+ * @param  {string} string
+ * @return {bool}
+ */
+function isStringRegex(string) {
+  return string[0] === "/" && string[string.length - 1] === "/";
+}
+
+/**
+ * Returns RegExp object for string like "/reg/"
+ *
+ * @param  {string} string
+ * @return {RegExp}
+ */
+function toRegex(string) {
+  return new RegExp(string.slice(1, -1));
+}
+
+/**
+ * Compares the declaration with regex pattern
+ * to verify the usage of variable
+ *
+ * @param  {string} val
+ * @return {bool}
+ */
+function checkValue(val, exceptions = []) {
+  // Regex for checking - capture 1 = function, 3 = variable
+  // $any-variable;
+  // any-function($any-variable
+  const regEx = /^([a-zA-Z0-9-]*)\(*(\$([A-Z0-9a-z-]+))/g;
+
+  for (const exception of exceptions) {
+    if (isStringRegex(exception)) {
+      if (toRegex(exception).test(val)) return true;
+    } else {
+      if (exception === val) return true;
+    }
+  }
+
+  const matches = regEx.exec(val);
+  if (matches && matches[3]) {
+    // if function check it's in themeFunctions
+    const passFunctionCheck =
+      matches[1].length === 0 || themeFunctions.includes(matches[1]);
+    // check token exists in theme
+    return passFunctionCheck && themesTokens.includes(matches[3]);
+  }
+  return false;
+}
+
+/**
+ * Checks the value and if its present in variables object
+ * returns the respective variable
+ *
+ * @param  {string}
+ * @return {string|bool}
+ */
+function checkPresentVariable(val) {
+  return variables[val] ? variables[val] : false;
+}
+
+/**
+ * Checks the defined property in (css|scss|less) with the
+ * test string or regex defined in stylelint config
+ *
+ * @param  {string} value
+ * @param  {string|regex} comparison
+ * @return {bool}
+ */
+function testAgaintString(prop, value, comparison) {
+  // if prop is a variable do not run check
+  // and add it in the variables object for later check
+  // and return, since it would be a variable declaration
+  // not a style property declaration
+  if (checkValue(prop)) {
+    variables[value] = prop;
+    return;
+  }
+
+  if (isStringRegex(comparison)) {
+    const valueMatches = new RegExp(comparison.slice(1, -1)).test(prop);
+    return valueMatches;
+  }
+
+  return prop == comparison;
+}
+
+/**
+ * Checks the test expression with css declaration
+ *
+ * @param  {string} prop
+ * @param  {string|array} comparison
+ * @return {bool}
+ */
+function checkProp(prop, value, targets) {
+  for (const target of targets) {
+    if (testAgaintString(prop, value, target)) return true;
+  }
+  return false;
+}
+
+/**
+ * Checks the test expression with css declaration
+ *
+ * @param  {string|array} options
+ * @return {object}
+ */
+function parseOptions(options) {
+  const parsed = { targets: [], ignoreValues: ["/color\\(/"] };
+
+  if (Array.isArray(options)) {
+    const last = options[options.length - 1];
+    if (typeof last === "object") {
+      parsed.targets = options.slice(0, options.length - 1);
+      if (last.ignoreValues) {
+        parsed.ignoreValues = parsed.ignoreValues.concat(last.ignoreValues);
+      }
+    } else {
+      parsed.targets = options;
+    }
+  } else {
+    parsed.targets = [options];
+  }
+  return parsed;
+}
+
+module.exports = stylelint.createPlugin(ruleName, function (config) {
+  options = parseOptions(config || []);
+
+  return function (root, result) {
+    const validOptions = stylelint.utils.validateOptions({
+      ruleName: ruleName,
+      result: result,
+      actual: options,
+    });
+
+    if (!validOptions) {
+      return;
+    }
+
+    root.walkDecls(function (statement) {
+      if (
+        checkProp(statement.prop, statement.value, options.targets) &&
+        checkPresentVariable(statement.value) &&
+        !checkValue(statement.value, options.ignoreValues)
+      ) {
+        stylelint.utils.report({
+          ruleName: ruleName,
+          result: result,
+          node: statement,
+          message: messages.expectedPresent(
+            statement.prop,
+            checkPresentVariable(statement.value)
+          ),
+        });
+      } else if (
+        checkProp(statement.prop, statement.value, options.targets) &&
+        !checkValue(statement.value, options.ignoreValues)
+      ) {
+        stylelint.utils.report({
+          ruleName: ruleName,
+          result: result,
+          node: statement,
+          message: messages.expected(statement.prop),
+        });
+      }
+    });
+  };
+});
+
+module.exports.ruleName = ruleName;
+module.exports.messages = messages;
